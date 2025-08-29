55219ab963fd50023f946236b4f9bf30e83c8eb5
li-jiang-yan <professional.jiangyan_li@outlook.com>
2025-08-29 23:05:44 +0300

    Added the scaling of feature data

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
 
 # Load dataset
 iris = datasets.load_wine()
 X = iris.data
 y = iris.target
+
+# Scale data
+X = StandardScaler().fit(X).transform(X)
